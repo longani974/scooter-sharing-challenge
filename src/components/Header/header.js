f95270e7f981424664ee01dcd94be13a5f1b7b62
Sylvain Rivière <sylv1.riviere@gmail.com>
2021-02-05 19:36:09 +0400

    max width 1440px & center

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import arrowImg from "../../assets/patterns/right-arrow.svg"
 import whiteCirclesImg from "../../assets/patterns/white-circles.svg"
+import line from "../../assets/patterns/line.svg"
 
 import * as styles from "./header.module.css"
 import Image from "../image/image"
@@ -9,10 +10,12 @@ import Image from "../image/image"
 const Header = props => {
   let spacingAround = styles.noContent
   let arrowToDisplay = <></>
+  let lineToDisplay = <></>
 
   if (props.isWithContent) {
     spacingAround = styles.withContent
     arrowToDisplay = <img className={styles.arrow} src={arrowImg} alt="" />
+    lineToDisplay = <img className={styles.line} src={line} alt="" />
   }
   return (
     <Image
@@ -22,10 +25,13 @@ const Header = props => {
       alt="Sunset Image"
     >
       <header className={styles.header + " " + spacingAround}>
-        <h1>{props.title}</h1>
-        <div>{props.content}</div>
-        {arrowToDisplay}
-        <img className={styles.whiteCirlces} src={whiteCirclesImg} alt="" />
+        <div>
+          <h1>{props.title}</h1>
+          <div>{props.content}</div>
+          {arrowToDisplay}
+          <img className={styles.whiteCirlces} src={whiteCirclesImg} alt="" />
+          {lineToDisplay}
+        </div>
       </header>
     </Image>
   )
