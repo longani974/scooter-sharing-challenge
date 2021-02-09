e83fe07dd5b3f9f51d868566bfa22af84b19546a
Sylvain Rivière <sylv1.riviere@gmail.com>
2021-02-09 17:21:06 +0400

    add: nav footer mobile

--- src/components/footNav/footNav.js
+++ src/components/footNav/footNav.js
@@ -1,8 +1,29 @@
+import { Link } from "gatsby"
 import React from "react"
+import Image from "../image/image"
+import LogoImg from "../../assets/logoWhite.svg"
 import * as styles from "./footNav.module.css"
 
 const FootNav = () => {
-  return <div>FootNav</div>
+  return (
+    <div className={styles.footNav}>
+      <div className={styles.logo}>
+        <Link to={"/"}>
+          <img fill="white" src={LogoImg} alt="logo"></img>
+        </Link>
+      </div>
+      <div className={styles.nav}>
+        <Link to={"/about/"}>About</Link>
+        <Link to={"/location/"}>Location</Link>
+        <Link to={"/careers/"}>Careers</Link>
+      </div>
+      <div className={styles.icons}>
+        <Image src="icons/facebook.svg" alt="" />
+        <Image src="icons/twitter.svg" alt="" />
+        <Image src="icons/instagram.svg" alt="" />
+      </div>
+    </div>
+  )
 }
 
 export default FootNav
